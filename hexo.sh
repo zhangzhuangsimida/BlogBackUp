fc93bafc458e81bbe67801a917ad11677e6e637c
zhangzhuang <2508031513@qq.com>
2021-02-02 01:47:01 +0800

    1 . 自动化调试脚本

--- hexo.sh
+++ hexo.sh
@@ -1,6 +1,27 @@
 #!/usr/bin/env sh
-echo "start debug"
-hexo cl
-hexo g
-hexo s
-echo "finish debug"
+# echo "start debug"
+# hexo cl
+# hexo g
+# hexo s
+# echo "finish debug"
+
+
+if [ "$1"x = "build"x ]; then
+  buildFun
+elif [ "$1"x = "server"x ]; then
+  buildFun
+  hexo s
+elif [ "$1"x = "publish"x ]; then
+  buildFun
+  hexo d
+else
+  echo "请输入正确的参数 （build,debug,deploy) $1"
+fi
+# echo "start debug"
+# hexo s
+# echo "finish debug"
+
+buildFun() {
+  hexo cl
+  hexo g
+}
